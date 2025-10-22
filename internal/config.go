1cc87fcee7f710e670ae2ab91176f9cf4bec1b07
daniil1412412 <daniil.kondratev.1337@gmail.com>
2025-10-22 21:09:57 +0500

    baseconf

---
+++ internal/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-default:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
