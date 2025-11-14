6b10641053861b80f8ed169486ba4d3bb5c691ee
daniil1412412 <daniil.kondratev.1337@gmail.com>
2025-11-14 17:51:46 +0500

    config

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-default:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := FetchConfigPath()
+	if path == "" {
+		panic("конфиг пустой")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("файл конфига не найден")
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("ошибка чтения конфига" + err.Error())
+	}
+
+	return &cfg
+}
+
+func FetchConfigPath() {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
